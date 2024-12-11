81906fd9231426697a8d44f8c675c2b7bf876abb
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2024-12-11 14:40:54 +0100

    Use arg for serve

--- cli/serve.ts
+++ cli/serve.ts
@@ -1,3 +1,4 @@
+import { normalize } from "node:path";
 import { computeNCRForRepositoryFiles, computeStream } from "../compute.ts";
 import express from "express";
 
@@ -6,9 +7,12 @@ const app = express();
 app.use(express.static("web/public"));
 app.use(express.static("build"));
 
+const targetDirectory = process.argv[2] ?? ".";
+const targetDirectoryNormalized = normalize(targetDirectory);
+
 app.use(async (req, res, next) => {
   if (req.path === "/data.json") {
-    res.json(await computeNCRForRepositoryFiles("C:/Users/jonas/p/git-truck"));
+    res.json(await computeNCRForRepositoryFiles(targetDirectoryNormalized));
   } else {
     next();
   }
