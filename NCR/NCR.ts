74bf9986d1e464a76e1ae7cb18e9a173402b7cc6
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2024-12-12 19:18:10 +0100

    Add assertions to validate buffer lengths in compression ratio calculation

--- NCR/NCR.ts
+++ NCR/NCR.ts
@@ -1,5 +1,6 @@
 import { readFile } from "fs/promises";
 import { compress } from "./compress.ts";
+import assert from "assert";
 
 /**
  * Calculate the normalized compression ratio for all files in a directory
@@ -31,23 +32,26 @@ export async function calculateNormalizedCompressionRatios(
     // 1. For each file, read it into memory
     const AFile = await readFile(f);
     const ABuffer = AFile;
-    const A = ABuffer.byteLength;
+    const A = ABuffer.length;
     ARawMap.set(f, ABuffer);
     // 2. Compress file
-    const _A_ = (await compress(ABuffer, "zstd")).byteLength;
+    const _A_ = (await compress(ABuffer, "zstd")).length;
     // 3. Store A and _A_ in a map
     AMap.set(f, A);
     _A_Map.set(f, _A_);
   }
 
   const fileBuffers = Array.from(ARawMap.values());
+  assert(fileBuffers.length === filePaths.length);
+
   const concatenatedBuffer = Buffer.concat(fileBuffers);
 
   // _AR_ is the size of the all files concatenated
-  const AR = concatenatedBuffer.byteLength;
+  const AR = concatenatedBuffer.length;
 
   // _AR_ is the compressed size of the all files concatenated
-  const _AR_ = (await compress(concatenatedBuffer, "zstd")).byteLength;
+  const _AR_ = (await compress(concatenatedBuffer, "zstd")).length;
+  assert(_AR_ < AR);
 
   // 3. For each file A, calculate the compression R for all files except A
   for (const f of filePaths) {
@@ -56,6 +60,10 @@ export async function calculateNormalizedCompressionRatios(
       .map(([_, buffer]) => buffer);
 
     const concatenatedBufferWithoutF = Buffer.concat(fileBuffersWithoutF);
+    assert(
+      concatenatedBufferWithoutF.length + AMap.get(f)! ==
+        concatenatedBuffer.length
+    );
     const compressizedBufferWithoutF = await compress(
       concatenatedBufferWithoutF,
       "zstd"
