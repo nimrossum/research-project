a24844244532a047140ea9079f67249f17617aca
joglr <1959615+joglr@users.noreply.github.com>
2025-02-07 18:40:32 +0100

    Add script to count file extensions in a directory

---
+++ cli/file-extension-count.ts
@@ -0,0 +1,18 @@
+import { getDirReader } from "@/utils/file";
+import { asyncIteratorToArray } from "@/utils/iterator";
+
+const dir = process.argv[2] || ".";
+
+const filePaths = await asyncIteratorToArray(getDirReader(dir));
+
+console.log(`✅ Found ${filePaths.length} files`);
+
+const differentFileExtensions = new Set(
+  filePaths.map((f) => f.split(".").pop())
+);
+!global.silent &&
+  console.log(
+    `👉 Found ${differentFileExtensions.size} extensions: ${Array.from(
+      differentFileExtensions
+    ).join(", ")}`
+  );
